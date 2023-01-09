a58f4ffc9ba9e25d730b356bba3f04c243ba16fc
notnulldev <61083452+NotNullDev@users.noreply.github.com>
2023-01-09 15:54:03 +0100

    upgraded error handling

--- api/nanoContext.ts
+++ api/nanoContext.ts
@@ -9,7 +9,7 @@ export function showEnv() {
 
 export async function fetchNanoContext(): Promise<NanoContext> {
   const res = await nanoFetch("/");
-  const data = (await res.json()) as NanoContext;
+  const data = (await res?.json()) as NanoContext;
   data.nanoConfig.globalEnvironment = base64Decode(
     data.nanoConfig.globalEnvironment
   );
@@ -25,7 +25,7 @@ export async function resetToken(): Promise<string> {
   const res = await nanoFetch("/reset-token", {
     method: "POST",
   });
-  let data = (await res.text()) as string;
+  let data = (await res?.text()) as string;
 
   // data comes in format "<token>", so we need to remove the quotes
   data = data.slice(1);
@@ -42,7 +42,7 @@ export async function updateGlobalEnv(updatedEnv: string): Promise<string> {
     },
     body: updatedEnv,
   });
-  const data = (await res.text()) as string;
+  const data = (await res?.text()) as string;
   return base64Decode(data);
 }
 
@@ -56,7 +56,7 @@ export async function createApp(name: string) {
       appName: name,
     }),
   });
-  const data = (await res.json()) as App;
+  const data = (await res?.json()) as App;
   return data;
 }
 
@@ -68,7 +68,7 @@ export async function updateApp(app: App) {
     },
     body: JSON.stringify(app),
   });
-  const data = (await res.json()) as App;
+  const data = (await res?.json()) as App;
   return data;
 }
 
@@ -76,7 +76,7 @@ export async function deleteApp(appId: number) {
   const res = await nanoFetch("/delete-app?id=" + appId, {
     method: "DELETE",
   });
-  const data = (await res.text()) as string;
+  const data = (await res?.text()) as string;
 
   return Number(data);
 }
@@ -88,7 +88,7 @@ export async function runBuild(appName: string) {
       Authorization: globalStore.getState().nanoConfig.token,
     },
   });
-  const data = (await res.text()) as string;
+  const data = (await res?.text()) as string;
   return data;
 }
 
@@ -103,7 +103,7 @@ export async function updateUser(username: string, password: string) {
       password: password,
     }),
   });
-  const data = (await res.text()) as string;
+  const data = (await res?.text()) as string;
   return data;
 }
 
@@ -118,7 +118,7 @@ export async function login(username: string, password: string) {
       password,
     }),
   });
-  let data = (await res.text()) as string;
+  let data = (await res?.text()) as string;
   // data comes in format "<token>", so we need to remove the quotes
   data = data.slice(1);
   data = data.slice(0, data.length - 2);
@@ -162,7 +162,8 @@ async function nanoFetch(path: string, options?: RequestInit) {
 
   if (!resp.ok) {
     const errMessage = await resp.json();
-    toast(errMessage.error);
+    toast.error(errMessage.error);
+    return;
     // throw new Error(resp.statusText);
   }
 
